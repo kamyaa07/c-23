00c8c6af51452aaba4265e2ee87af16ba5b48188
kamyaa07 <53122015+kamyaa07@users.noreply.github.com>
2020-12-26 11:29:23 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,21 @@
+class Box
+{
+    constructor()
+    {
+        var options=
+        {
+            restitution:1.0
+        }
+        this.body= Bodies.rectangle(200,100,50,50,options);
+        World.add(world,this.body);
+    }
+
+
+    display()
+    {
+      var pos=this.body.position;  
+      rectMode(CENTER);
+      fill("yellow");
+      rect(pos.x,pos.y,this.width,this.height);
+    }
+}
